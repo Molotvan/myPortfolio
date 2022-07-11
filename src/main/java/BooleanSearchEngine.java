a795d44f52c4a7f0be04ca5220b09968a31653dc
Molotvan <molotvan@yandex.ru>
2022-07-11 18:05:38 +0300

    toComp

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,4 +1,10 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+
+import javax.print.Doc;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
@@ -6,13 +12,28 @@ public class BooleanSearchEngine implements SearchEngine {
     //???
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
-    }
+        File pdfs = new File("pdfs");
+        File[] pdfsList = pdfs.listFiles();
+        for (File pdf : pdfsList) {
+            try {FileReader reader = new FileReader(pdf);
+                 reader.read();
+                PdfDocument pdfFile = new PdfDocument(new PdfReader(pdf));
+                PdfPage[] filePages = new PdfPage[pdfFile.getNumberOfPages()];
+                for (int i=0; i<pdfFile.getNumberOfPages(); i++) {
+                    filePages[i] = pdfFile.getPage(i);
 
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+    }
     @Override
-    public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+    public List<PageEntry> search(String word){
+        return null;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+        //BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+//        System.out.println(engine.search("бизнес"));
+//        FileReader reader = new FileReader("pdfs");
+        File pdfs = new File("pdfs");
+        File[] pdfsList = pdfs.listFiles();
+        for (File pdf : pdfsList) {
+//            FileReader reader = new FileReader(pdf);
+//                reader.read();
+                PdfDocument pdfFile = new PdfDocument(new PdfReader(pdf));
+            System.out.println(pdf.getName());
+//            }catch (IOException e){
+//                e.printStackTrace();
+//            }
 
+        }
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
